0fd35e0a36b4524d486a8899c7744ddfc67fb2a3
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-11-04 16:10:49 -0200

    Aula 3

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify, render_template
-
+import requests
 
 app = Flask(__name__) ##__name__ pega nome do diretorio atual
 
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
+   # return 'hello world'
 
 @app.route('/api')
 def minha_api():
     mensagem = {"mensagem": "api rest"}
     return jsonify(mensagem)
 
+@app.route('/cep/<string:busca>')
+def buscar_cep(busca):
+    data = requests.get('https://viacep.com.br/ws/{}/json/'.format(busca))
+    return jsonify(data.json())
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
