efab5ca76ad3e52213e3d3c07e44fc1ac2267280
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-11-04 15:51:44 -0200

    Aula 3

--- bla/flask/app.py
+++ bla/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask
 
 
 app = Flask(__name__) ##__name__ pega nome do diretorio atual
@@ -7,12 +7,6 @@ app = Flask(__name__) ##__name__ pega nome do diretorio atual
 @app.route('/')
 def index():
     return 'hello world'
-
-@app.route('/api')
-def minha_api():
-    mensagem = {"mensagem": "api rest"}
-    return jsonify(mensagem)
-
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
